78164e4de26c6d918665556beae37ff448422209
sjawahar <selvakumarj@diipl.com>
2019-01-30 22:48:04 +0530

    First Commit

---
+++ PytorchProjectsCpp/CatClassifier/src/main.cpp
@@ -0,0 +1,11 @@
+#include<torch/torch.h>
+#include <torch/cuda.h>
+#include <iostream>
+
+int main() {
+	
+	torch::Tensor tensor = torch::rand({ 2, 3 });
+	std::cout << tensor << std::endl;
+	std::cout << torch::cuda::is_available()<<'\n';
+
+}
